a98f6852e259c191488d63899c106d9ec6200943
Prince <103827570+onukwilip@users.noreply.github.com>
2023-01-10 00:50:20 +0100

    Enabled Eslint ignoring builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   env: {
     KEY: "uEKBcN7kMKayW6SF8d0BtaJq60Musbp0",
     VECTOR: "hA7wB3e4v87ihj6R",
